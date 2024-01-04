bb98076751ba25b66154c017d668ae128c82950d
Naomi_Jalil <110171806+Naomi-Jalil@users.noreply.github.com>
2024-01-04 22:20:32 +0600

    Created userModel

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require('colors');
 const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected to Mongodb database ${mongoose.connection.host}`); 
+        console.log(`Connected to Mongodb database ${mongoose.connection.host}`.bgMagenta.white); 
     }
     catch(error){
         console.log(`MongoDB Database Error ${error}`.bgRed.white);
